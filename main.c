c9ed29c93b729fc52f7e4849a3e25b99eff11b14
Sahil Shaikh <sahilpk81@gmail.com>
2023-01-02 19:27:16 +0530

    added define procedure

--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 int expanding;
-FILE* iptr;
+FILE *iptr,*exptr,*namptr,*defptr;
 char label[100],opcode[100],operand[100];
 void getLine();
 void processLine();
-
+void define();
+void clear_def_and_nam_tab();
+void expand();
 
 int main()
 {
-
+    clear_def_and_nam_tab();
     iptr = fopen("inputprogram.txt","r");
+    exptr = fopen("expandedfile.txt","w");
+
     while(strcmp(opcode,"END")!=0)
     {
         getLine();
-        //processline
+        processLine();
 
     }
+    fclose(exptr);
 
 }
 
@@ -34,5 +39,49 @@ void getLine(){
 
 void processLine()
 {
+    if(strcmp(opcode,"MACRO")==0)
+    {
+        define();
+    }
+    else{
+    fprintf(exptr,"%s\t%s\t%s\n",label,opcode,operand);
+    }
+}
 
+void define()
+{
+
+    namptr = fopen("namtab.txt","a");
+    defptr = fopen("deftab.txt","a");
+    fprintf(namptr,"%s",label);
+    fprintf(defptr,"%s\t%s\t%s\n",label,opcode,operand);
+    int level = 1;
+    while(level>0){
+            getLine();
+            //todo sub positional notation
+            fprintf(defptr,"%s\t%s\t%s\n",label,opcode,operand);
+             if(strcmp(opcode,"MACRO")==0){
+                level++;
+             }
+             else if(strcmp(opcode,"MEND")==0){
+                    level--;
+             }
+    }
+    fclose(namptr);
+    fclose(defptr);
+    //todo store pointers
+
+}
+
+void expand()
+{
+    expanding = 1;
+}
+
+void clear_def_and_nam_tab()
+{
+    namptr = fopen("namtab.txt","w");
+    defptr = fopen("deftab.txt","w");
+    fclose(namptr);
+    fclose(defptr);
 }
