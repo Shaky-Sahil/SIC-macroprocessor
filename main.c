f2141deb4cf2e5a157d711faf53c33373c2fe467
Sahil Shaikh <sahilpk81@gmail.com>
2023-01-02 18:32:14 +0530

    added getline procedure

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-int exapanding=0;
+#include <string.h>
+int expanding;
+FILE* iptr;
+char label[100],opcode[100],operand[100];
+void getLine();
+void processLine();
+
+
 int main()
 {
 
-    printf("Hello world!\n");
-    return 0;
+    iptr = fopen("inputprogram.txt","r");
+    while(strcmp(opcode,"END")!=0)
+    {
+        getLine();
+        //processline
+
+    }
+
+}
+
+void getLine(){
+    if(expanding == 1)
+    {
+        printf("reading from deftab");
+    }
+    else{
+        fscanf(iptr,"%s%s%s",label,opcode,operand);
+        //printf("the label is %s the opcode is %s the operand is %s\n",label,opcode,operand);
+    }
+}
+
+void processLine()
+{
+
 }
